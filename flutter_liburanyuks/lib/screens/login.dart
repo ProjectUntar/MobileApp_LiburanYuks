e299cf6295091c4a6752d3ad3a5b6d5a80e870e3
BrianWij <88884241+BrianWij@users.noreply.github.com>
2022-03-15 14:37:16 +0700

    yo

---
+++ flutter_liburanyuks/lib/screens/login.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'main_screen.dart';
+import 'register.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.all(40),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  child: Image(
+                    height: 200,
+                    width: 200,
+                    image: AssetImage("assets/images/objects/Logo1.png"),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Liburan Yuks',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email Address',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 70,
+                    padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text(
+                        'Login',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                       Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => MainScreen()));
+                      },
+                    )),
+                Container(
+                    child: Row(
+                  children: <Widget>[
+                    Text('Does not have account?'),
+                    FlatButton(
+                      textColor: Colors.blue,
+                      child: Text(
+                        'Register',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => RegisterPage()));
+                      },
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ))
+              ],
+            )));
+  }
+}
